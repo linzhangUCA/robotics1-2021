4bb8d95af1fb64a21f44e32686e8a7640d82dd5a
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2021-08-26 08:03:47 -0500

    Update examples.py

--- 0826/examples.py
+++ 0826/examples.py
@@ -1,6 +1,33 @@
-# control flow
-x = 12
+# if statement
+x = input("Please enter a number: ")
 if x>0:
     print("positive")
+elif x==0:
+    print("zero")    
 else:
     print("negative")
+
+# for loop
+for i in range(10):
+    print(i)
+    
+# another for loop
+robots = ['atlas', 'r2d2', 'gundam-rx-78-2', 'roomba']
+for robot in robots:
+    print("name: {}, name length: {}".format(robot, len(robot)))
+    
+# while loop
+theta = -3.1415926
+while theta < 3.1415926:
+    print(theta)
+    theta = theta + 0.5 # theta += 0.5
+    if int(theta)==0:
+        theta = 1
+    if theta >= 2.5:
+        print("{}, time to break".format(theta))
+        break
+
+# define function
+def add_two_number(a,b):
+    c = a + b
+    return c
